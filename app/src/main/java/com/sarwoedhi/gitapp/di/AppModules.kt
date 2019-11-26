c6b187e0710741c4a7ec28de04990ecb11914ed9
Sarwoedhi <pedhitia@gmail.com>
2019-11-26 22:13:32 +0700

    Add SearchRepositoriesViewModel

--- app/src/main/java/com/sarwoedhi/gitapp/di/AppModules.kt
+++ app/src/main/java/com/sarwoedhi/gitapp/di/AppModules.kt
@@ -3,6 +3,7 @@ package com.sarwoedhi.gitapp.di
 import com.sarwoedhi.gitapp.data.source.api.GithubAPI
 import com.sarwoedhi.gitapp.data.source.repo.AppRepository
 import com.sarwoedhi.gitapp.ui.contributor.UserContributorViewModel
+import com.sarwoedhi.gitapp.ui.search.SearchRepositoriesViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -10,5 +11,5 @@ val AppModules = module {
     single { GithubAPI() }
     factory { AppRepository(get()) }
     viewModel { UserContributorViewModel(get()) }
-
+    viewModel { SearchRepositoriesViewModel(get()) }
 }
