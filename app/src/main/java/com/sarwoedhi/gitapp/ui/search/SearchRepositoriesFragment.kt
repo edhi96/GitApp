1d8c66ed69711e7803621006257aea8bb25c08cb
Sarwoedhi <pedhitia@gmail.com>
2019-11-26 22:23:21 +0700

    add search feature

--- app/src/main/java/com/sarwoedhi/gitapp/ui/search/SearchRepositoriesFragment.kt
+++ app/src/main/java/com/sarwoedhi/gitapp/ui/search/SearchRepositoriesFragment.kt
@@ -5,21 +5,85 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sarwoedhi.gitapp.R
+import kotlinx.android.synthetic.main.fragment_search_repositories.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 
-/**
- * A simple [Fragment] subclass.
- */
 class SearchRepositoriesFragment : Fragment() {
 
+    private val searchViewModel: SearchRepositoriesViewModel by sharedViewModel()
+    private lateinit var searchAdapter: SearchAdapter
+    private lateinit var searchView: SearchView
+    private lateinit var mProgressBarSearch: ProgressBar
+    private lateinit var recyclerSearch: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_repositories, container, false)
+        val view = inflater.inflate(R.layout.fragment_search_repositories, container, false)
+        recyclerSearch = view.rvResultSearchList
+        mProgressBarSearch = view.progressBarSearch
+        mProgressBarSearch.visibility = View.INVISIBLE
+        searchAdapter = SearchAdapter(requireContext())
+        searchView = view.searchRepos
+
+        val textChangeListener: SearchView.OnQueryTextListener =
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText.isNullOrEmpty()) {
+                        searchAdapter.resetResults()
+                        mProgressBarSearch.visibility = View.GONE
+                    }
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    if (query != null) {
+                        if (query.length > 2) {
+                            mProgressBarSearch.visibility = View.VISIBLE
+                            loadDataSearch(query)
+                        }
+                    }
+                    return true
+                }
+            }
+        searchView.setOnQueryTextListener(textChangeListener)
+        recyclerSearch.layoutManager = LinearLayoutManager(requireContext())
+        recyclerSearch.setHasFixedSize(true)
+        return view
     }
 
 
+    private fun loadDataSearch(q: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val dataSearch = searchViewModel.getResultSearch(q)
+            withContext(Dispatchers.Main) {
+                dataSearch.observe(requireActivity(), Observer {
+                    if (!it.isNullOrEmpty()) {
+                        searchAdapter.resetResults()
+                        recyclerSearch.adapter = searchAdapter
+                        searchAdapter.setResults(it)
+                        recyclerSearch.adapter = searchAdapter
+                        mProgressBarSearch.visibility = View.GONE
+                    } else {
+                        mProgressBarSearch.visibility = View.VISIBLE
+                    }
+                })
+            }
+        }
+    }
+
 }
+
+
