2b1ab6995e939d31fc327e3d4435b069c0efcb01
Sarwoedhi <pedhitia@gmail.com>
2019-11-26 18:11:04 +0700

    Add Modules DI GithubAPI AppRepository UserContributorViewModel

---
+++ app/src/main/java/com/sarwoedhi/gitapp/di/AppModules.kt
@@ -0,0 +1,14 @@
+package com.sarwoedhi.gitapp.di
+
+import com.sarwoedhi.gitapp.data.source.api.GithubAPI
+import com.sarwoedhi.gitapp.data.source.repo.AppRepository
+import com.sarwoedhi.gitapp.ui.contributor.UserContributorViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val AppModules = module {
+    single { GithubAPI() }
+    factory { AppRepository(get()) }
+    viewModel { UserContributorViewModel(get()) }
+
+}
