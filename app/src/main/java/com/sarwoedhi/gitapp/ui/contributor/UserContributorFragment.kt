b52a76350eb32427b7c022c63758664a44431740
Sarwoedhi <pedhitia@gmail.com>
2019-11-26 18:12:47 +0700

    Add UserContributorFragment and make getContributors from github

--- app/src/main/java/com/sarwoedhi/gitapp/ui/contributor/UserContributorFragment.kt
+++ app/src/main/java/com/sarwoedhi/gitapp/ui/contributor/UserContributorFragment.kt
@@ -2,24 +2,57 @@ package com.sarwoedhi.gitapp.ui.contributor
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sarwoedhi.gitapp.R
+import kotlinx.android.synthetic.main.fragment_user_contributor.view.*
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class UserContributorFragment : Fragment() {
-
+    private lateinit var recycleContributes: RecyclerView
+    private lateinit var mAdapter: ContributorAdapter
+    private lateinit var mProgressBar: ProgressBar
+    private val mViewModel: UserContributorViewModel by sharedViewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_contributor, container, false)
+        val view = inflater.inflate(R.layout.fragment_user_contributor, container, false)
+
+        recycleContributes = view.rvContributors
+        mProgressBar = view.progressBar
+
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.let {
+            mAdapter = ContributorAdapter(it)
+            loadData()
+        }
+    }
 
+    private fun loadData() {
+        val dataUsers = mViewModel.data
+        dataUsers.observe(requireActivity(), Observer {
+            if (!it.isNullOrEmpty()) {
+                mAdapter.setUsersData(it)
+                mAdapter.notifyDataSetChanged()
+                mProgressBar.visibility = View.GONE
+                recycleContributes.adapter = mAdapter
+                recycleContributes.layoutManager = LinearLayoutManager(requireContext())
+                recycleContributes.setHasFixedSize(true)
+            } else {
+                mProgressBar.visibility = View.VISIBLE
+            }
+        })
+    }
 }
